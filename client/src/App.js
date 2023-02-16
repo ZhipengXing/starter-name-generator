d7105370912836423c64af674d25ec1e276446b2
ZhipengXing <dadofrankpeng@gmail.com>
2023-02-15 20:17:53 -0600

    first update

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,16 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 const App = () => {
-  const [name, setName] = useState([])
+  const [name, setName] = useState([]);
 
   useEffect(() => {
-    fetch('https://enigmatic-shore-92552.herokuapp.com/generate')
+    fetch("https://starter-name-generator-backend-mdcj.onrender.com/generate")
       .then((res) => res.json())
       .then((nme) => setName([nme]))
-  }, [])
+      .catch((error) => console.log(error));
+  }, []);
 
-  return (
-    <h1>{name}</h1>
-  )
-}
+  return <h1>{name}</h1>;
+};
 
-export default App
+export default App;
